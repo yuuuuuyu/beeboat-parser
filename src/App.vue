b819141b48506a167f099a273155b2eef4fe3506
于智勇 <zhiyong.yu@ytever.com>
2024-07-17 09:17:00 +0800

    refactor: 1

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
-    <el-config-provider size="small">
+    <el-config-provider size="large" :locale="zhCn">
         <router-view></router-view>
     </el-config-provider>
 </template>
 
 <script lang="ts" setup>
+import zhCn from 'element-plus/dist/locale/zh-cn'
 import beeTheme from 'beeboat-plus/es/beeboat-theme'
 beeTheme.start()
 </script>
